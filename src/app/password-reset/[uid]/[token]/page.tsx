5206874b103fce4012765b5d2972cf4358e65543
lucalvex <lucalvex25@gmail.com>
2025-04-29 10:53:40 -0300

    correção do Props v.3

--- src/app/password-reset/[uid]/[token]/page.tsx
+++ src/app/password-reset/[uid]/[token]/page.tsx
@@ -13,18 +13,20 @@ interface Props {
     };
 }
 
-export default async function Page({ params }: Props) {
-    const {uid, token } = params;
+// Função de página assíncrona no Next.js 13+ (App Directory)
+export default async function Page({ params }: { params: { uid: string; token: string } }) {
+    const { uid, token } = params;
+
     return (
-        <main className='min-h-screen p-12'>
-            <div className='flex flex-col max-w-md mx-auto bg-gunmetal rounded-md border-2 border-blue-darknut'>
-                <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-                    <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight'>
+        <main className="min-h-screen p-12">
+            <div className="flex flex-col max-w-md mx-auto bg-gunmetal rounded-md border-2 border-blue-darknut">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">
                         Redefina sua senha
                     </h2>
                 </div>
 
-                <div className='m-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+                <div className="m-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <PasswordResetConfirmForm uid={uid} token={token} />
                 </div>
             </div>
