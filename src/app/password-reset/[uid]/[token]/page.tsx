9431f6c59b6a6e127279c847b2bdae07208bf566
lucalvex <lucalvex25@gmail.com>
2025-04-29 10:51:39 -0300

    correção do Props v.2

--- src/app/password-reset/[uid]/[token]/page.tsx
+++ src/app/password-reset/[uid]/[token]/page.tsx
@@ -13,7 +13,7 @@ interface Props {
     };
 }
 
-export default function Page({ params }: Props) {
+export default async function Page({ params }: Props) {
     const {uid, token } = params;
     return (
         <main className='min-h-screen p-12'>
